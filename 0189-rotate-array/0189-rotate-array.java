b400d8187a47e411e58c07d7771b9d6f21011540
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-02-06 02:50:14 +0530

    Time: 0 ms (100%), Space: 61.3 MB (68.38%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -14,33 +14,50 @@ class Solution {
         // for(int i=0; i<n; i++){
         //   nums[i]=arr[i];
         // }
-        int temp;
-        int i=0,j=n-k-1;
-        while(i<j)
+       
+        // int temp; //optimal
+        // int i=0,j=n-k-1;
+        // while(i<j)
+        // {
+        //     temp=nums[i];
+        //     nums[i]=nums[j];
+        //     nums[j]=temp;
+        //     i++;
+        //     j--;
+        // }
+        // int l=n-k,m=n-1;
+        // while(l<m)
+        // {
+        //      temp=nums[l];
+        //     nums[l]=nums[m];
+        //     nums[m]=temp;
+        //     l++;
+        //     m--;
+        // }
+        // int o=0,p=n-1;
+        //  while(o<p)
+        // {
+        //      temp=nums[o];
+        //     nums[o]=nums[p];
+        //     nums[p]=temp;
+        //     o++;
+        //     p--;
+        // }
+        reverse(nums,0,n-k-1);
+        reverse(nums,n-k,n-1);
+        reverse(nums,0,n-1);
+    }
+     public void reverse(int[] nums,int i,int j)
         {
+            int temp;
+             while(i<j)
+         {
             temp=nums[i];
             nums[i]=nums[j];
             nums[j]=temp;
             i++;
             j--;
+         }
+
         }
-        int l=n-k,m=n-1;
-        while(l<m)
-        {
-             temp=nums[l];
-            nums[l]=nums[m];
-            nums[m]=temp;
-            l++;
-            m--;
-        }
-        int o=0,p=n-1;
-         while(o<p)
-        {
-             temp=nums[o];
-            nums[o]=nums[p];
-            nums[p]=temp;
-            o++;
-            p--;
-        }
-    }
 }
