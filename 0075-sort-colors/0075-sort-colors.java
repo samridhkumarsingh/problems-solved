3ed89f3678af4a23ac882b30f65f2a2ff477079c
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-02-08 00:39:20 +0530

    Time: 0 ms (100%), Space: 43.8 MB (21.69%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,33 +1,58 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int n=nums.length;      //method 1
-        int noOfZeros=0,noOfOne=0;
+        // int n=nums.length;      //method 1
+        // int noOfZeros=0,noOfOne=0;
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(nums[i]==0)
+        //     {
+        //         noOfZeros++;
+        //     }
+        //     else if(nums[i]==1)
+        //     {
+        //         noOfOne++;
+        //     }
+        // }
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(i<noOfZeros)
+        //     {
+        //         nums[i]=0;
+        //     }
+        //     else if(i<noOfZeros+noOfOne)
+        //     {
+        //         nums[i]=1;
+        //     }
+        //     else
+        //     {
+        //         nums[i]=2;
+        //     }
+        // }
+        int n= nums.length;
+        int l=0,m=0,h=n-1;
         for(int i=0;i<n;i++)
         {
-            if(nums[i]==0)
+            int temp;
+            if(nums[m]==0)
             {
-                noOfZeros++;
+                temp=nums[l];
+                nums[l]=nums[m];
+                nums[m]=temp;
+
+                m++;
+                l++;
             }
-            else if(nums[i]==1)
+            else if(nums[m]==1)
             {
-                noOfOne++;
-            }
-        }
-        for(int i=0;i<n;i++)
-        {
-            if(i<noOfZeros)
-            {
-                nums[i]=0;
-            }
-            else if(i<noOfZeros+noOfOne)
-            {
-                nums[i]=1;
+               m++;
             }
             else
             {
-                nums[i]=2;
+                temp=nums[h];
+                nums[h]=nums[m];
+                nums[m]=temp;
+                h--;
             }
         }
-        
     }
 }
