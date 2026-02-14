b60eca4be987e97f5a0e59877d0aa68dc0590a9f
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-02-14 12:29:24 +0530

    Time: 1 ms (94.75%), Space: 43.6 MB (31.7%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,23 +1,36 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> ans=new ArrayList<>();
+        // List<List<Integer>> ans=new ArrayList<>(); //brute force
+        // for(int i=0;i<numRows;i++)
+        // {
+        //     List<Integer> l=new ArrayList<>();
+        //     for(int j=0;j<=i;j++)
+        //     {
+        //         l.add(1);
+        //     }
+        //     ans.add(l);
+        // }
+
+        // for(int i=2;i<numRows;i++)
+        // {
+        //     for(int j=1;j<i;j++)
+        //     {
+        //       //  ans.get(i).get(j)=ans.get(i-1).get(j)+ans.get(i-1).get(j-1); set use krna hoga n; get se value mil ra hai bs.
+        //       ans.get(i).set(j,ans.get(i-1).get(j)+ans.get(i-1).get(j-1));
+        //     }
+        // }
+        // return ans;
+
+        List<List<Integer>> ans=new ArrayList<>(); //optimal solution
         for(int i=0;i<numRows;i++)
         {
             List<Integer> l=new ArrayList<>();
             for(int j=0;j<=i;j++)
             {
-                l.add(1);
+               if(j==0 || j==i) l.add(1);
+               else l.add(ans.get(i-1).get(j)+ans.get(i-1).get(j-1));
             }
             ans.add(l);
-        }
-
-        for(int i=2;i<numRows;i++)
-        {
-            for(int j=1;j<i;j++)
-            {
-              //  ans.get(i).get(j)=ans.get(i-1).get(j)+ans.get(i-1).get(j-1); set use krna hoga n; get se value mil ra hai bs.
-              ans.get(i).set(j,ans.get(i-1).get(j)+ans.get(i-1).get(j-1));
-            }
         }
         return ans;
     }
