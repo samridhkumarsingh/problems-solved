42d7bc04b26096ed70547dd8558dcaca9449f182
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-02-14 12:20:31 +0530

    Time: 2 ms (11.19%), Space: 43.6 MB (51.08%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans=new ArrayList<>();
+        for(int i=0;i<numRows;i++)
+        {
+            List<Integer> l=new ArrayList<>();
+            for(int j=0;j<=i;j++)
+            {
+                l.add(1);
+            }
+            ans.add(l);
+        }
+
+        for(int i=2;i<numRows;i++)
+        {
+            for(int j=1;j<i;j++)
+            {
+              //  ans.get(i).get(j)=ans.get(i-1).get(j)+ans.get(i-1).get(j-1); set use krna hoga n; get se value mil ra hai bs.
+              ans.get(i).set(j,ans.get(i-1).get(j)+ans.get(i-1).get(j-1));
+            }
+        }
+        return ans;
+    }
+}
