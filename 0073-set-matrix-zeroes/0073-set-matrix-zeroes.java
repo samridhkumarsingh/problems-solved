62bd23fea0b819c27d4a4a6b4cf6430ae56773de
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-02-18 12:58:45 +0530

    Time: 0 ms (100%), Space: 47.5 MB (30.09%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,33 +1,69 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        //method 1 //brute force
         int m=matrix.length,n=matrix[0].length;
-        int[][] helper =new int[m][n];
-        for(int i=0;i<m;i++)
-        {for(int j=0;j<n;j++)
-        {
-            helper[i][j]=matrix[i][j];
-        }}
+        
+        // //method 1 //brute force
+        // int[][] helper =new int[m][n];
+        // for(int i=0;i<m;i++)
+        // {for(int j=0;j<n;j++)
+        // {
+        //     helper[i][j]=matrix[i][j];
+        // }}
+        // for(int i=0;i<m;i++)
+        // {
+        //     for(int j=0;j<n;j++)
+        //     {
+        //         if(helper[i][j]==0)
+        //         {
+        //             //for row
+        //             for(int k=0;k<n;k++)
+        //             {
+        //                 matrix[i][k]=0;
+        //             }
+
+        //             //for colom
+        //             for(int p=0;p<m;p++)
+        //             {
+        //                 matrix[p][j]=0;
+        //             }
+        //         }
+        //     }
+        // }
+        
+
+        //method 2
+        boolean[] row=new boolean[m];
+        boolean[] colomn=new boolean[n];
         for(int i=0;i<m;i++)
         {
             for(int j=0;j<n;j++)
             {
-                if(helper[i][j]==0)
+                if(matrix[i][j]==0)
                 {
-                    //for row
-                    for(int k=0;k<n;k++)
-                    {
-                        matrix[i][k]=0;
-                    }
-
-                    //for colom
-                    for(int p=0;p<m;p++)
-                    {
-                        matrix[p][j]=0;
-                    }
+                    row[i]=true;
+                    colomn[j]=true;
                 }
             }
         }
-        
+        for(int i=0;i<m;i++)
+        {
+            if(row[i]==true)
+            {
+                for(int a=0;a<n;a++)
+                {
+                    matrix[i][a]=0;
+                }
+            }
+        }
+           for(int j=0;j<n;j++)
+        {
+            if(colomn[j]==true)
+            {
+                for(int b=0;b<m;b++)
+                {
+                    matrix[b][j]=0;
+                }
+            }
+        }
     }
 }
