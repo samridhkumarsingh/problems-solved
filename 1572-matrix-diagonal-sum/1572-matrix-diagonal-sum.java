c67a2d01a52cb0a7e5c52c819c5a32dcb9821ada
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-02-20 01:24:57 +0530

    Time: 0 ms (100%), Space: 46.6 MB (12.37%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.java
@@ -3,22 +3,33 @@ class Solution {
         int m=mat.length;
         int n=mat[0].length;
         int sum=0;
-        for(int i=0;i<m;i++)
+        // for(int i=0;i<m;i++) //brute force
+        // {
+        //     for(int j=0;j<n;j++)
+        //     {
+        //         if(i==j)
+        //         {
+        //             sum+=mat[i][j];
+        //         }
+        //         if(i+j==m-1)
+        //         {
+        //             if(i!=j)
+        //             {
+        //                   sum+=mat[i][j];
+        //             }
+        //         }
+        //     }
+        // }
+
+        for(int i=0;i<m;i++)//optial sol
         {
-            for(int j=0;j<n;j++)
-            {
-                if(i==j)
-                {
-                    sum+=mat[i][j];
-                }
-                if(i+j==m-1)
-                {
-                    if(i!=j)
-                    {
-                          sum+=mat[i][j];
-                    }
-                }
-            }
+            sum+=(mat[i][i]+mat[i][n-1-i]);
+        }
+
+        //if odd 
+        if(m%2==1)
+        {
+            sum-=(mat[n/2][n/2]);
         }
         return sum;
     }
