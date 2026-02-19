cfe7c50c25f0d2af1dc84674f532617333775395
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-02-19 23:59:58 +0530

    Time: 0 ms (100%), Space: 47.7 MB (16.53%) - LeetHub

--- 0566-reshape-the-matrix/0566-reshape-the-matrix.java
+++ 0566-reshape-the-matrix/0566-reshape-the-matrix.java
@@ -3,29 +3,35 @@ class Solution {
         int m=mat.length;
         int n=mat[0].length;
         int[][] arr=new int[r][c];
-        if(m*n==r*c)
-        {
-            int p=0,q=0;
-            for(int i=0;i<m;i++)
-            {
-                for(int j=0;j<n;j++)
-                {
-                    if(q==c)
-                    {
-                      p++;
-                        q=0;
-                        arr[p][q]=mat[i][j];
-                        q++;
-                      
-                    }else
-                    {
-                    arr[p][q]=mat[i][j];
-                    q++;
-                    }
-                }
-            }
-            return arr;
-        }
-        return mat;
+        // if(m*n==r*c) //brute force
+        // {
+        //     int p=0,q=0;
+        //     for(int i=0;i<m;i++)
+        //     {
+        //         for(int j=0;j<n;j++)
+        //         {
+        //             if(q==c)
+        //             {
+        //               p++;
+        //                 q=0;
+        //             }
+                    
+        //             arr[p][q]=mat[i][j];
+        //             q++;
+                    
+        //         }
+        //     }
+        //     return arr;
+        // }
+        // return mat;
+
+
+       if(m*n!=r*c) return mat;//optimal
+       for(int i=0;i<m*n;i++)
+       {
+        arr[i/c][i%c]=mat[i/n][i%n];
+       } 
+
+       return arr;
     }
 }
