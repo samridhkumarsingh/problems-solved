16817b33c20d65f74dfcb68e386817dbc7d76320
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-02-25 23:59:09 +0530

    Time: 2 ms (27.81%), Space: 45.3 MB (90.33%) - LeetHub

--- 0443-string-compression/0443-string-compression.java
+++ 0443-string-compression/0443-string-compression.java
@@ -1,20 +1,21 @@
 class Solution {
     public int compress(char[] chars) {
-        String ans="";
+        // String ans=""; string java mei bekar hai iss liye stringbuilder use kr lete hai
+        StringBuilder ans=new StringBuilder("");
         int i=0,j=0;
         while(j<chars.length)
         {
             if(chars[j]==chars[i]) j++;
             else{
-                ans+=chars[i];
+                ans.append(chars[i]);
                 int leng=j-i;
-                if(leng>1) ans+=leng;
+                if(leng>1) ans.append(leng);
                 i=j;
             }
         }
-        ans+=chars[i];
+        ans.append(chars[i]);
                 int leng=j-i;
-                if(leng>1) ans+=leng;
+                if(leng>1) ans.append(leng);
                 i=j;
 
         for(int k=0;k<ans.length();k++)
