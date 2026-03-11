7c08a9df0a7062628300169e35e4f02912db706d
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-03-11 21:34:40 +0530

    Time: 2 ms (90.36%), Space: 47.9 MB (54.37%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,35 +1,37 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-      int noz=0;
-      int n=nums.length;
-      for(int ele:nums)
-      {
-        if(ele==0) noz++;
+    //   int noz=0; //bubble sort aqlgo
+    //   int n=nums.length;
+    //   for(int ele:nums)
+    //   {
+    //     if(ele==0) noz++;
 
-      }
-      for(int x=0;x<noz;x++)
-      {
-        for(int i=0;i<n-1-x;i++)
+    //   }
+    //   for(int x=0;x<noz;x++)
+    //   {
+    //     for(int i=0;i<n-1-x;i++)
+    //     {
+    //         if(nums[i]==0)
+    //         {
+    //             int temp=nums[i];
+    //             nums[i]=nums[i+1];
+    //             nums[i+1]=temp;
+    //         }
+    //     }
+    //   }
+
+
+        int pos=0;
+        for(int i=0;i<nums.length;i++)
         {
-            if(nums[i]==0)
-            {
-                int temp=nums[i];
-                nums[i]=nums[i+1];
-                nums[i+1]=temp;
-            }
+        if(nums[i]!=0)
+        {
+            int temp=nums[pos];
+            nums[pos]=nums[i];
+           nums[i]=temp;
+            pos++;
         }
-      }
-        // int pos=0;
-        // for(int i=0;i<nums.length;i++)
-        // {
-        // if(nums[i]!=0)
-        // {
-        //     int temp=nums[pos];
-        //     nums[pos]=nums[i];
-        //    nums[i]=temp;
-        //     pos++;
-        // }
-        // }
+        }
         
     }
 }
