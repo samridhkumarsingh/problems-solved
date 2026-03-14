e176242b09936b9a5a61bbbe6cdc4209c89fa5be
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-03-14 23:31:50 +0530

    Time: 544 ms (5%), Space: 47.4 MB (50.98%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -4,7 +4,20 @@ class Solution {
         for(int i=0;i<nums.length;i++){
             nums[i]=nums[i]*nums[i];
         }
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
+        int n=nums.length;
+        for(int i=0;i<n-1;i++) //bubble sort
+        {
+            for(int j=0;j<n-1-i;j++)
+            {
+              if(nums[j]>nums[j+1])
+              {
+                int temp=nums[j];
+                nums[j]=nums[j+1];
+                nums[j+1]=temp;
+              }
+            }
+        }
         return nums;
     }
 }
