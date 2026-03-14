c2c6c77c16c0f8977fa7cbd9c22434ef993b8a33
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-03-14 23:24:55 +0530

    Time: 0 ms (100%), Space: 47 MB (45.66%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.java
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.java
@@ -1,18 +1,31 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
         int n=nums.length;
-        for(int i=0;i<n-1;i++)
+        // for(int i=0;i<n-1;i++)//bubble sort
+        // {
+        //     for(int j=0;j<n-1-i;j++)
+        //     {
+        //         if(nums[j]%2!=0)
+        //         {
+        //             int temp=nums[j];
+        //             nums[j]=nums[j+1];
+        //             nums[j+1]=temp;
+        //         }
+        //     }
+        // }
+        // return nums;
+        int p=0;
+        for(int i=0;i<n;i++)
         {
-            for(int j=0;j<n-1-i;j++)
+            if(nums[i]%2==0)
             {
-                if(nums[j]%2!=0)
-                {
-                    int temp=nums[j];
-                    nums[j]=nums[j+1];
-                    nums[j+1]=temp;
-                }
+                int temp=nums[i];
+                nums[i]=nums[p];
+                nums[p]=temp;
+                p++;
             }
         }
+
         return nums;
     }
 }
