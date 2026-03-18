97039884086410243ac1a5848404316cfb701f77
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-03-18 20:19:03 +0530

    Time: 3 ms (79.75%), Space: 43.2 MB (50.94%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -2,35 +2,58 @@ class Solution {
     public List<String> summaryRanges(int[] nums) {
         ArrayList<String> arr=new ArrayList<>();
         int n=nums.length;
-        int p=0,q=0;
-        if(n==0) return arr;
-      while(p<n-1)
-      {
-        if(nums[p]+1==nums[p+1])
-        {
-            p++;
-        }else
-        {
-            if(p!=q)
-            {
-                arr.add(nums[q]+"->"+nums[p]);
+         if(n==0) return arr;
+
+    //     int p=0,q=0;
+    //   while(p<n-1)
+    //   {
+    //     if(nums[p]+1==nums[p+1])
+    //     {
+    //         p++;
+    //     }else
+    //     {
+    //         if(p!=q)
+    //         {
+    //             arr.add(nums[q]+"->"+nums[p]);
                
+    //         }else
+    //         {
+    //             arr.add(Integer.toString(nums[q]));
+    //         }
+    //          p=p+1;
+    //          q=p;
+    //     }
+    //   }
+    //    if(p!=q)
+    //         {
+    //             arr.add(nums[q]+"->"+nums[p]);
+               
+    //         }else
+    //         {
+    //             arr.add(Integer.toString(nums[q]));
+    //         }
+    //   return arr;
+
+    int start=nums[0];
+    for(int i=1;i<n;i++)
+    {
+        if(nums[i]!=nums[i-1]+1)
+        {
+            if(nums[i-1]==start)
+            {
+                arr.add(Integer.toString(start));
             }else
             {
-                arr.add(Integer.toString(nums[q]));
+                arr.add(start+"->"+nums[i-1]);
             }
-             p=p+1;
-             q=p;
+            start=nums[i];
         }
-      }
-       if(p!=q)
-            {
-                arr.add(nums[q]+"->"+nums[p]);
-               
-            }else
-            {
-                arr.add(Integer.toString(nums[q]));
-            }
-      return arr;
+    }
+        if(start == nums[n-1])
+            arr.add(String.valueOf(start));
+        else
+            arr.add(start + "->" + nums[n-1]);
+
+        return arr;
     }
 }
