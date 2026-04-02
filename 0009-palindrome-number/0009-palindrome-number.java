9b7d8e700d175a1932ce07594d7acd74be1489a7
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-04-02 23:04:49 +0530

    Time: 5 ms (79.84%), Space: 45.8 MB (80.7%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x<0||(x%10==0&&x!=0)) return false; //
+        if(x<0||(x%10==0&&x!=0)) return false; /////
         int rev=0;
         int n=x;
         while(n>0)
