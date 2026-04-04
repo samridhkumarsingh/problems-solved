88e6bbf4ac8db4067acfae6834dab5b249e4a095
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-04-04 12:10:33 +0530

    Time: 0 ms (100%), Space: 42.3 MB (9.67%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int cum(int n,int[] dp)
+    {
+        if(n==0) return 1;
+        if(n==1) return 1;
+        if(dp[n]!=-1) return dp[n];
+        dp[n]=cum(n-1,dp)+cum(n-2,dp);
+        return dp[n];
+    }
+    public int climbStairs(int n) {
+        int[] dp=new int[n+1];
+        for(int i=0;i<n+1;i++)
+        {
+            dp[i]=-1;
+        }
+
+        return cum(n,dp);
+        
+    }
+}
