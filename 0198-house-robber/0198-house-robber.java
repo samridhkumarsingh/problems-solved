dee248d1170b362a8a8587711d1e22f04f96fea7
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-04-04 12:30:44 +0530

    Time: 0 ms (100%), Space: 42.8 MB (45.78%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,32 +1,17 @@
 class Solution {
-  int MaxAm(int idx, int[] nums, int[] dp)
+    public int mxrb(int indx,int[] nums,int[] dp)
     {
-        int n = nums.length;
+        if(indx>=nums.length) return 0;
+        if(dp[indx]!=-1) return dp[indx];
+        int rob=nums[indx]+mxrb(indx+2,nums,dp);
+        int skip=mxrb(indx+1,nums,dp);
+        dp[indx]=Math.max(rob,skip);
+        return dp[indx];
 
-        if(idx >= n)
-        {
-            return 0;
-        }
-
-        if(dp[idx] != -1)   // already calculated
-        {
-            return dp[idx];
-        }
-
-        int rob = nums[idx] + MaxAm(idx+2, nums, dp);
-        int skip = MaxAm(idx+1, nums, dp);
-
-        dp[idx] = Math.max(rob, skip);
-
-        return dp[idx];
     }
-
     public int rob(int[] nums) {
-        int n = nums.length;
-        int dp[] = new int[n];
-
+        int[] dp=new int[nums.length+1];
         Arrays.fill(dp,-1);
-
-        return MaxAm(0, nums, dp);
+        return mxrb(0,nums,dp);
     }
 }
