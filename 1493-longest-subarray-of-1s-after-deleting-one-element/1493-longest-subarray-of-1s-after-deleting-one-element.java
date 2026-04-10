15b41287e79a9d4f32645fb8c23aed0da591c215
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-04-10 23:34:39 +0530

    Time: 3 ms (87.56%), Space: 65.2 MB (93.07%) - LeetHub

--- 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.java
+++ 1493-longest-subarray-of-1s-after-deleting-one-element/1493-longest-subarray-of-1s-after-deleting-one-element.java
@@ -1,27 +1,46 @@
 class Solution {
     public int longestSubarray(int[] nums) {
+        // int right=0,left=0;
+        // int n=nums.length;
+        // int c=0,max=0;
+        // HashMap<Integer,Integer> map=new HashMap<>();
+        
+        // while(right<n)
+        // {
+        //     if(map.containsKey(nums[right]))
+        //     {
+        //         c=right-left-1;
+        //         max=Math.max(c,max);
+        //         left=map.get(nums[right])+1;
+        //     }
+        //     if(nums[right]==0)
+        //     {
+        //         map.put(nums[right],right);
+        //     }
+        //     right++;
+        // }
+        // c=right-left-1;
+        // max=Math.max(c,max);
+
+        // return max;
+
         int right=0,left=0;
         int n=nums.length;
         int c=0,max=0;
-        HashMap<Integer,Integer> map=new HashMap<>();
-        
+        int noZero=0;
         while(right<n)
         {
-            if(map.containsKey(nums[right]))
+            if(nums[right]==0) noZero++;
+            while(noZero>1)
             {
-                c=right-left-1;
-                max=Math.max(c,max);
-                left=map.get(nums[right])+1;
+                if(nums[left]==0) noZero--;
+                left++;
             }
-            if(nums[right]==0)
-            {
-                map.put(nums[right],right);
-            }
-            right++;
-        }
-        c=right-left-1;
-        max=Math.max(c,max);
+             c=right-left;
+             max=Math.max(c,max);
+             right++;
 
+        }
         return max;
     }
 }
