c9ac081dbe5f69af21a69a2e3f96ccd3206f0059
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-06-12 14:26:12 +0530

    Time: 0 ms (100%), Space: 42.4 MB (5.05%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,14 +1,7 @@
 class Solution {
-    // public int fib(int n) {
-    //     if(n<=1) return n;
-    //     // if(n==1) return 1;
-    //     // if(n==0) return 0;        
-    //     return fib(n-1)+fib(n-2);
-    // }
-
     public int fibo(int n,int[] dp)
     {
-        if(n<=1) return n;  //dp with extra array
+        if(n<=1) return n;
 
         if(dp[n]!=-1) return dp[n];
         int last=fibo(n-1,dp);
@@ -16,8 +9,12 @@ class Solution {
         dp[n]=last+slast;
         return dp[n];
     }
-    public int fib(int n)
-    {
+
+    public int fib(int n) {
+        // if(n<=1) return n;
+        // int last=fib(n-1);
+        // int slast=fib(n-2);
+        // return last+slast;
         int[] dp=new int[n+1];
         Arrays.fill(dp,-1);
         return fibo(n,dp);
