8180d83e72e23d17ea867be9d78e7e9bceb1f2db
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-06-15 01:05:44 +0530

    Time: 2 ms (21.75%), Space: 61.3 MB (40.09%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,63 +1,33 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        int  n=nums.length; 
+        int n =nums.length;
         k=k%n;
-        // int[] arr=new int[n];//brute force
-        // for(int i=0;i<k;i++)
-        // {
-        //     arr[i]=nums[n-k+i];
-        // }
-        // for(int j=0;j<n-k;j++)
-        // {
-        //     arr[k+j]=nums[j];
-        // }
-        // for(int i=0; i<n; i++){
-        //   nums[i]=arr[i];
-        // }
-       
-        // int temp; //optimal
-        // int i=0,j=n-k-1;
-        // while(i<j)
-        // {
-        //     temp=nums[i];
-        //     nums[i]=nums[j];
-        //     nums[j]=temp;
-        //     i++;
-        //     j--;
-        // }
-        // int l=n-k,m=n-1;
-        // while(l<m)
-        // {
-        //      temp=nums[l];
-        //     nums[l]=nums[m];
-        //     nums[m]=temp;
-        //     l++;
-        //     m--;
-        // }
-        // int o=0,p=n-1;
-        //  while(o<p)
-        // {
-        //      temp=nums[o];
-        //     nums[o]=nums[p];
-        //     nums[p]=temp;
-        //     o++;
-        //     p--;
-        // }
-        reverse(nums,0,n-k-1);
-        reverse(nums,n-k,n-1);
-        reverse(nums,0,n-1);
-    }
-     public void reverse(int[] nums,int i,int j)
+        int i=0,j=n-k-1;
+        while(i<j)
         {
-            int temp;
-             while(i<j)
-         {
-            temp=nums[i];
+            int temp=nums[i];
             nums[i]=nums[j];
             nums[j]=temp;
             i++;
             j--;
-         }
-
         }
+        int l=n-k,m=n-1;
+         while(l<m)
+        {
+            int temp=nums[l];
+            nums[l]=nums[m];
+            nums[m]=temp;
+            l++;
+            m--;
+        }
+        int p=0,q=n-1;
+         while(p<q)
+        {
+            int temp=nums[p];
+            nums[p]=nums[q];
+            nums[q]=temp;
+            p++;
+            q--;
+        }
+    }
 }
