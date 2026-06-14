0ca006e13dbff7703b020b21d94aae6af7e86df2
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-06-14 23:56:30 +0530

    Time: 0 ms (100%), Space: 43 MB (42.79%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,38 +1,17 @@
 class Solution {
     public boolean check(int[] nums) {
-    //    int smll=Integer.MAX_VALUE;   //wrong approach
-    //    int indx=0;
-    //    int n=nums.length;
-    //    for(int i=0;i<n;i++) 
-    //    {
-    //     if(nums[i]<smll)
-    //     {
-    //         smll=nums[i];
-    //         indx=i;
-    //     }
-    //    }    
-    //     for(int i=indx;i<n-1;i++)
-    //     {
-    //         if(nums[i]>nums[i+1]) return false;
-    //     }
-    //    if(indx==0) return true;
-    //     if(nums[n-1]>nums[0]) return false;
-    //     for(int i=0;i<indx-1;i++)
-    //     {
-    //          if(nums[i]>nums[i+1]) return false;
-    //     }
+        int c=0;
+        int n=nums.length-1;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]-nums[i+1]>0)
+            {
+                c++;
+            }
+        }
+        if(nums[n]-nums[0]>0) c++;
 
-    //     return true;
-
-    int count=0;
-    int n=nums.length;
-    for(int i=0;i<n-1;i++)
-    {
-        if(nums[i]>nums[i+1]) count++;
-    }
-
-    if(nums[0]<nums[n-1]) count++;
-
-    return count<=1;
+        if(c<2) return true;
+        return false;
     }
 }
