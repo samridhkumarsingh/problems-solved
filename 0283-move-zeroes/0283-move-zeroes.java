53a00111ce14d4a00a4048b7fcd3631f651ac3f3
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-06-15 22:50:04 +0530

    Time: 2 ms (91.91%), Space: 47.7 MB (74.94%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,37 +1,15 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-    //   int noz=0; //bubble sort aqlgo
-    //   int n=nums.length;
-    //   for(int ele:nums)
-    //   {
-    //     if(ele==0) noz++;
-
-    //   }
-    //   for(int x=0;x<noz;x++)
-    //   {
-    //     for(int i=0;i<n-1-x;i++)
-    //     {
-    //         if(nums[i]==0)
-    //         {
-    //             int temp=nums[i];
-    //             nums[i]=nums[i+1];
-    //             nums[i+1]=temp;
-    //         }
-    //     }
-    //   }
-
-
-        int pos=0;
+        int p=0;
         for(int i=0;i<nums.length;i++)
         {
-        if(nums[i]!=0)
-        {
-            int temp=nums[pos];
-            nums[pos]=nums[i];
-           nums[i]=temp;
-            pos++;
+            if(nums[i]!=0)
+            {
+                int temp=nums[i];
+                nums[i]=nums[p];
+                nums[p]=temp;
+                p++;
+            }
         }
-        }
-        
     }
 }
