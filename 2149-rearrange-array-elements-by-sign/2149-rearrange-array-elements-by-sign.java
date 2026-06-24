6b5341119eb09f1100efee14466a8d47d15d850b
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-06-24 17:11:37 +0530

    Time: 3 ms (100%), Space: 175.5 MB (56.49%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,3 +1,20 @@
+// class Solution {
+//     public int[] rearrangeArray(int[] nums) {
+//         int p=0,n=1;
+//        while(p<nums.length && n<nums.length)
+//        {
+//         if(nums[p]>=0) p+=2;
+//         if(nums[n]<0)n+=2;
+//         if((p<nums.length && n<nums.length)&&(nums[p]<0 && nums[n]>=0))
+//         {
+//             int temp=nums[p];
+//             nums[p]=nums[n];
+//             nums[n]=temp;
+//         }
+//        }
+//        return nums;
+//     }
+// }
 class Solution {
     public int[] rearrangeArray(int[] nums) {
         int e=0,o=1;
