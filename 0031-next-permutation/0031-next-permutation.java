be5c8ba5eef398881cd5f12647538542bd565832
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-06-25 15:39:28 +0530

    Time: 1 ms (10.6%), Space: 45.1 MB (5.01%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,7 +1,7 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int n=nums.length;
         int index=-1;
+        int n=nums.length;
         for(int i=n-2;i>=0;i--)
         {
             if(nums[i]<nums[i+1])
@@ -9,13 +9,13 @@ class Solution {
                 index=i;
                 break;
             }
-            
         }
         if(index==-1)
         {
             Arrays.sort(nums);
             return;
         }
+        //get the no. greater then index
         for(int i=n-1;i>index;i--)
         {
             if(nums[i]>nums[index])
@@ -25,17 +25,18 @@ class Solution {
                 nums[index]=temp;
                 break;
             }
+            
         }
+        //reverse the rest element to make them shorter
         int left=index+1;
         int right=n-1;
         while(left<right)
         {
-           int temp= nums[left];
-           nums[left]=nums[right];
-           nums[right]=temp;
-           left++;
-           right--;
+             int temp=nums[left];
+                nums[left]=nums[right];
+                nums[right]=temp;
+            left++;
+            right--;
         }
-        
     }
 }
