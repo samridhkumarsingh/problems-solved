1241d7e9781aa9004c129742fbbb597543fdfdbb
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-06-29 00:22:35 +0530

    Time: 30 ms (76.92%), Space: 95.9 MB (16.85%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,25 +1,47 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
+        // int lng=0;
+        // int count=1;
+        // if(nums.length==0) return 0;
+        // int lastmin=Integer.MIN_VALUE;
+        // Arrays.sort(nums);
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     if(nums[i]-1==lastmin)
+        //     {
+        //         count++;
+        //         lastmin=nums[i];
+        //     }
+        //     else if(nums[i]-1!=lastmin && nums[i]!=lastmin)
+        //     {
+        //         lastmin=nums[i];
+        //         lng=Math.max(lng,count);
+        //         count=1;
+        //     }
+        // }
+        // lng=Math.max(lng,count);
+        // return lng;
+        HashSet <Integer> keys=new HashSet<>();
         int lng=0;
-        int count=1;
-        if(nums.length==0) return 0;
-        int lastmin=Integer.MIN_VALUE;
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length;i++)
+        for(int i:nums)
         {
-            if(nums[i]-1==lastmin)
-            {
-                count++;
-                lastmin=nums[i];
-            }
-            else if(nums[i]-1!=lastmin && nums[i]!=lastmin)
-            {
-                lastmin=nums[i];
-                lng=Math.max(lng,count);
-                count=1;
-            }
+            keys.add(i);
         }
-        lng=Math.max(lng,count);
-        return lng;
+        for(int i:keys)
+        {
+            if(!keys.contains(i-1))
+            {
+                int count=1;
+                int curr=i;
+                while(keys.contains(curr+1))
+                {
+                    count++;
+                    curr++;
+                }
+                lng=Math.max(count,lng);
+            }
+           
+        }
+         return lng;
     }
 }
