e989c6cc6f541ae875ae89b5223ba80b54ce9643
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-06-30 22:54:42 +0530

    Time: 0 ms (100%), Space: 47.8 MB (25.31%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -1,99 +1,21 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-        int m=matrix.length,n=matrix[0].length;
-        
-        // //method 1 //brute force
-        // int[][] helper =new int[m][n];
-        // for(int i=0;i<m;i++)
-        // {for(int j=0;j<n;j++)
-        // {
-        //     helper[i][j]=matrix[i][j];
-        // }}
-        // for(int i=0;i<m;i++)
-        // {
-        //     for(int j=0;j<n;j++)
-        //     {
-        //         if(helper[i][j]==0)
-        //         {
-        //             //for row
-        //             for(int k=0;k<n;k++)
-        //             {
-        //                 matrix[i][k]=0;
-        //             }
-
-        //             //for colom
-        //             for(int p=0;p<m;p++)
-        //             {
-        //                 matrix[p][j]=0;
-        //             }
-        //         }
-        //     }
-        // }
-        
-
-        // //method 2
-        // boolean[] row=new boolean[m];
-        // boolean[] colomn=new boolean[n];
-        // for(int i=0;i<m;i++)
-        // {
-        //     for(int j=0;j<n;j++)
-        //     {
-        //         if(matrix[i][j]==0)
-        //         {
-        //             row[i]=true;
-        //             colomn[j]=true;
-        //         }
-        //     }
-        // }
-        // for(int i=0;i<m;i++)
-        // {
-        //     if(row[i]==true)
-        //     {
-        //         for(int a=0;a<n;a++)
-        //         {
-        //             matrix[i][a]=0;
-        //         }
-        //     }
-        // }
-        //    for(int j=0;j<n;j++)
-        // {
-        //     if(colomn[j]==true)
-        //     {
-        //         for(int b=0;b<m;b++)
-        //         {
-        //             matrix[b][j]=0;
-        //         }
-        //     }
-        // }
-
-        // method3
-        boolean ZeroRow=false;
-        boolean ZeroCol=false;
-
-        //check the zero row
-        for(int j=0;j<n;j++)
+        boolean row=false;
+        boolean col=false;
+        int n=matrix.length;
+        int m=matrix[0].length;
+        for(int j=0;j<m;j++)
         {
-            if(matrix[0][j]==0)
-            {
-                ZeroRow=true;
-                break;
-            }
+           if(matrix[0][j]==0) row=true;
+        }
+        for(int i=0;i<n;i++)
+        {
+           if(matrix[i][0]==0) col=true;
         }
 
-          //check the zero colomn
-        for(int i=0;i<m;i++)
+        for(int i=1;i<n;i++)
         {
-            if(matrix[i][0]==0)
-            {
-                ZeroCol=true;
-                break;
-            }
-        }
-
-        //checking the matrix without zero row and colomn
-        for(int i=1;i<m;i++)
-        {
-            for(int j=1;j<n;j++)
+            for(int j=1;j<m;j++)
             {
                 if(matrix[i][j]==0)
                 {
@@ -102,48 +24,40 @@ class Solution {
                 }
             }
         }
-
-        //if zero convating the rows and colomn zero
-
-        //for row
-        for(int j=1;j<n;j++)
+        for(int j=1;j<m;j++)
         {
-            if(matrix[0][j]==0)
+           if(matrix[0][j]==0)
+           {
+            for(int i=1;i<n;i++)
             {
-                for(int i=1;i<m;i++)
-                {
-                    matrix[i][j]=0;
-                }
+                matrix[i][j]=0;
             }
+           }
         }
-
-         //for colomn
-        for(int i=1;i<m;i++)
+         for(int i=1;i<n;i++)
         {
             if(matrix[i][0]==0)
             {
-                for(int j=1;j<n;j++)
-                {
-                    matrix[i][j]=0;
-                }
+            for(int j=1;j<m;j++)
+            {
+                matrix[i][j]=0;
+            }
             }
         }
 
-        //travling zero rows and colomn
-        if(ZeroRow==true)
+        if(row==true)
         {
-            for(int j=0;j<n;j++)
+            for(int j=0;j<m;j++)
             {
                 matrix[0][j]=0;
             }
         }
-        if(ZeroCol==true)
+          if(col==true)
         {
-            for(int i=0;i<m;i++)
+            for(int i=0;i<n;i++)
             {
                 matrix[i][0]=0;
             }
         }
-
     }
 }
