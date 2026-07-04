bbe42360bdbb89566fcb210d313338b4e8cfb9c5
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-07-04 13:10:25 +0530

    Time: 2 ms (94.31%), Space: 72.1 MB (40.31%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,25 +1,41 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int i=1;i<nums.length;i++)
-        {
-            map.put(i,map.getOrDefault(i-1,1)*nums[i-1]);
-        }
+        // HashMap<Integer,Integer> map=new HashMap<>(); //extra space using hashMap
+        // for(int i=1;i<nums.length;i++)
+        // {
+        //     map.put(i,map.getOrDefault(i-1,1)*nums[i-1]);
+        // }
 
-             int temp=nums[nums.length-1];
-        for(int i=nums.length-1;i>0;i--)
-        {
-           int temp2=nums[i];
-            nums[i]=map.get(i);
-            if(i<nums.length-1)
-            {
+        //      int temp=nums[nums.length-1];
+        // for(int i=nums.length-1;i>0;i--)
+        // {
+        //    int temp2=nums[i];
+        //     nums[i]=map.get(i);
+        //     if(i<nums.length-1)
+        //     {
              
-             nums[i]=nums[i]*temp;
-            temp*=temp2;
-            }
-        }
-        nums[0]=temp;
+        //      nums[i]=nums[i]*temp;
+        //     temp*=temp2;
+        //     }
+        // }
+        // nums[0]=temp;
 
-        return nums;
+        // return nums;
+
+        int n=nums.length;
+        int[] ans=new int[n];
+        ans[0]=1;
+        for(int i=1;i<n;i++)
+        {
+            ans[i]=ans[i-1]*nums[i-1];
+        }
+        int temp=nums[n-1];
+        for(int i=n-2;i>=0;i--)
+        {
+            ans[i]*=temp;
+            temp*=nums[i];
+        }
+
+        return ans;
     }
 }
