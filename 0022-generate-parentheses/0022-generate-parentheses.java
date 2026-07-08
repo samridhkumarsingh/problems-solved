460cd653e4f1e0016cc0555f8b6a71d0d637a0d7
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-07-08 15:59:30 +0530

    Time: 2 ms (71.45%), Space: 44.9 MB (33.62%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -4,7 +4,7 @@ class Solution {
         sub(0,0,"",n,list);
         return list;
     }
-    public static void sub(int open,int close,String ans, int n,List<String> list)
+    public  void sub(int open,int close,String ans, int n,List<String> list)
     {   
       
         if(ans.length()==(n*2))
@@ -17,7 +17,8 @@ class Solution {
             sub(open+1,close,ans+'(',n,list);
           
         }
-        if(ans.length()!=0 && close<open)
+      //  if(ans.length()!=0 && close<open)
+        if(close<open)
         {
               sub(open,close+1,ans+')',n,list);
              
