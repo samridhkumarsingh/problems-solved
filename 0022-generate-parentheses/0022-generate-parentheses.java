65e556e92398632eb0dd40d5cbefb5a82df45a5e
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-07-08 15:58:09 +0530

    Time: 2 ms (71.45%), Space: 45.1 MB (9.06%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> list=new ArrayList<>();
+        sub(0,0,"",n,list);
+        return list;
+    }
+    public static void sub(int open,int close,String ans, int n,List<String> list)
+    {   
+      
+        if(ans.length()==(n*2))
+        {
+            list.add(ans);
+            return;
+        }
+        if(open<n)
+        {
+            sub(open+1,close,ans+'(',n,list);
+          
+        }
+        if(ans.length()!=0 && close<open)
+        {
+              sub(open,close+1,ans+')',n,list);
+             
+        }
+    }
+
+}
