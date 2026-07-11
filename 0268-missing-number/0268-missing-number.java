f31691f5ef35661cdda6b368cb2077f38053b883
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-07-11 16:49:32 +0530

    Time: 1 ms (29.85%), Space: 47.3 MB (48.42%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -8,17 +8,45 @@ class Solution {
         //     cal+=nums[i];
         // }
         // return sum-cal;
-        int n=nums.length;
-        boolean[] b=new boolean[n+1];
-        for(int ele:nums)
-        {
-            b[ele]=true;
-        }
-        for(int i=0;i<=n;i++)
-        {
-            if(b[i]==false) return i;
-        }
 
-        return 0;
+
+        // int n=nums.length;   //extra space
+        // boolean[] b=new boolean[n+1];
+        // for(int ele:nums)
+        // {
+        //     b[ele]=true;
+        // }
+        // for(int i=0;i<=n;i++)
+        // {
+        //     if(b[i]==false) return i;
+        // }
+
+        // return 0;
+
+        int n=nums.length;
+        int i=0;
+        while(i<n)
+        {
+           
+            if(nums[i]==i || nums[i]==n)
+            {
+            i++;
+            }
+            else
+            {
+                swap(i,nums[i],nums);
+            }
+        }
+        for(i=0;i<n;i++)
+        {
+            if(nums[i]!=i) return i;
+        }
+        return i;
+    }
+    public void swap(int i,int j,int[] nums)
+    {
+        int temp=nums[i];
+        nums[i]=nums[j];
+        nums[j]=temp;
     }
 }
