6eaeb7fee8124b46f570a3d30154dcbf6146bbd1
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-07-11 16:24:02 +0530

    Time: 0 ms (100%), Space: 47.2 MB (65.67%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,12 +1,24 @@
 class Solution {
     public int missingNumber(int[] nums) {
+        // int n=nums.length;  by sum 
+        // int sum=(n*(n+1))/2;
+        // int cal=0;
+        // for(int i=0;i<n;i++)
+        // {
+        //     cal+=nums[i];
+        // }
+        // return sum-cal;
         int n=nums.length;
-        int sum=(n*(n+1))/2;
-        int cal=0;
-        for(int i=0;i<n;i++)
+        boolean[] b=new boolean[n+1];
+        for(int ele:nums)
         {
-            cal+=nums[i];
+            b[ele]=true;
         }
-        return sum-cal;
+        for(int i=0;i<=n;i++)
+        {
+            if(b[i]==false) return i;
+        }
+
+        return 0;
     }
 }
