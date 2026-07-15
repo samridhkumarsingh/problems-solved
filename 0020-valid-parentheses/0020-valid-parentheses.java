97b80efecce4a0a7b20f27e785891d6673677109
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-07-16 00:54:07 +0530

    Time: 3 ms (87.05%), Space: 43.4 MB (26.4%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,59 +1,86 @@
-class Solution {
-     ArrayList<Character> arr;
-     int p;
-     int max;
-    public boolean isValid(String s) { //
-        arr=new ArrayList<>();
-         p=0;
-        max=s.length();
+// class Solution {
+//      ArrayList<Character> arr;
+//      int p;
+//      int max;
+//     public boolean isValid(String s) { //
+//         arr=new ArrayList<>();
+//          p=0;
+//         max=s.length();
      
-        for(int i=0;i<max;i++)
+//         for(int i=0;i<max;i++)
+//         {
+//             char c=s.charAt(i);
+//             if(c=='('||c=='['||c=='{'){
+//                 push(c);
+//             }else{
+//                 if(p == 0) return false;
+//                 char d=peek();
+//                 if(c==')'){
+//                     if(d=='(')
+//                     {
+//                         pop();
+//                     }
+//                   else{
+//                     return false;
+//                     }
+//                 }else if(c==']'){
+//                     if(d=='[')
+//                     {
+//                        pop();
+//                     }
+//                   else{
+//                     return false;
+//                     }
+//                 } else if(c=='}'){
+//                     if(d=='{')
+//                     {
+//                         pop();
+//                     }
+//                   else{
+//                     return false;
+//                     }
+//                 } 
+//             }
+//         }
+//  if(p==0) return true;
+//         else return false;
+//     }
+//     public void push(char x) {
+//         arr.add(x);
+//         p++;
+//     }
+//       public char peek() {
+//         return arr.get(p-1);
+//     }
+//     public void pop() {
+//         arr.remove(p-1);
+//         p--;
+//     }
+// }
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character>st=new Stack<>();
+        int n=s.length();
+        for(int i=0;i<n;i++)
         {
             char c=s.charAt(i);
-            if(c=='('||c=='['||c=='{'){
-                push(c);
-            }else{
-                if(p == 0) return false;
-                char d=peek();
-                if(c==')'){
-                    if(d=='(')
-                    {
-                        pop();
-                    }
-                  else{
-                    return false;
-                    }
-                }else if(c==']'){
-                    if(d=='[')
-                    {
-                       pop();
-                    }
-                  else{
-                    return false;
-                    }
-                } else if(c=='}'){
-                    if(d=='{')
-                    {
-                        pop();
-                    }
-                  else{
-                    return false;
-                    }
-                } 
+            if(c=='(')
+            {
+                st.push(')');
+            }
+            else if(c=='{')
+            {
+                st.push('}');
+            }
+             else if(c=='[')
+            {
+                st.push(']');
+            }
+            else
+            {
+                if(st.isEmpty()|| st.pop()!=c) return false;
             }
         }
- if(p==0) return true;
-        else return false;
-    }
-    public void push(char x) {
-        arr.add(x);
-        p++;
-    }
-      public char peek() {
-        return arr.get(p-1);
-    }
-    public void pop() {
-        arr.remove(p-1);
-        p--;
+        return st.isEmpty();
     }
 }
