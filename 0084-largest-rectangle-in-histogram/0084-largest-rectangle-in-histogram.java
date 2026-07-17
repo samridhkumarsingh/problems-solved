2e71192d4132561049b4635a4337b128c67acedb
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-07-17 15:34:38 +0530

    Time: 109 ms (19.63%), Space: 77.7 MB (70.13%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.java
@@ -1,43 +1,99 @@
+// class Solution {
+//     public int largestRectangleArea(int[] heights) {
+//         int n=heights.length;
+//         int[] lse=new int[n];
+//         int[] rse=new int[n];
+//         Stack<Integer> stl=new Stack<>();
+//         Stack<Integer> str=new Stack<>();
+//         for(int i=0;i<n;i++) //left smaller element
+//         {
+          
+//             while(!stl.isEmpty()&& heights[stl.peek()]>=heights[i])
+//             {
+//                 stl.pop();
+//             }
+//             if(stl.isEmpty())
+//             {
+//                 lse[i]=0;
+//             }
+//             else
+//             {
+//                 lse[i]=stl.peek()+1;
+//             }
+//             stl.push(i);
+//         }
+//           for(int i=n-1;i>=0;i--) //right smaller element
+//         {
+          
+//             while(!str.isEmpty()&& heights[str.peek()]>=heights[i])
+//             {
+//                 str.pop();
+//             }
+//             if(str.isEmpty())
+//             {
+//                 rse[i]=n-1; //last tak ja skta hai na 
+//             }
+//             else
+//             {
+//                 rse[i]=str.peek()-1;
+//             }
+//             str.push(i);
+//         }
+
+//         int max=0;
+//         for(int i=0;i<n;i++)
+//         {
+//             max=Math.max(max,((rse[i]-lse[i])+1)*heights[i]);
+//         }
+//         return max;
+//     }
+// }
+
 class Solution {
     public int largestRectangleArea(int[] heights) {
         int n=heights.length;
         int[] lse=new int[n];
         int[] rse=new int[n];
-        Stack<Integer> stl=new Stack<>();
-        Stack<Integer> str=new Stack<>();
+        Stack<Integer> st=new Stack<>();
         for(int i=0;i<n;i++) //left smaller element
         {
           
-            while(!stl.isEmpty()&& heights[stl.peek()]>=heights[i])
+            while(!st.isEmpty()&& heights[st.peek()]>=heights[i])
             {
-                stl.pop();
+                st.pop();
             }
-            if(stl.isEmpty())
+            if(st.isEmpty())
             {
                 lse[i]=0;
             }
             else
             {
-                lse[i]=stl.peek()+1;
+                lse[i]=st.peek()+1;
             }
-            stl.push(i);
+            st.push(i);
         }
+
+        while(!st.isEmpty())  //making stack empty for another use
+        {
+            st.pop();
+        }
+
           for(int i=n-1;i>=0;i--) //right smaller element
         {
           
-            while(!str.isEmpty()&& heights[str.peek()]>=heights[i])
+            while(!st.isEmpty()&& heights[st.peek()]>=heights[i])
             {
-                str.pop();
+                st.pop();
             }
-            if(str.isEmpty())
+            if(st.isEmpty())
             {
-                rse[i]=n-1;
+                rse[i]=n-1; //last tak ja skta hai na 
             }
             else
             {
-                rse[i]=str.peek()-1;
+                rse[i]=st.peek()-1;
             }
-            str.push(i);
+            st.push(i);
         }
 
         int max=0;
