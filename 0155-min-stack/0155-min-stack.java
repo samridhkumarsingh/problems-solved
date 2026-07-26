69845a25856ad12885989e53c8b85efdd5af2b0e
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-07-27 02:44:26 +0530

    Time: 33 ms (64.73%), Space: 100.1 MB (39.52%) - LeetHub

--- 0155-min-stack/0155-min-stack.java
+++ 0155-min-stack/0155-min-stack.java
@@ -1,37 +1,37 @@
-class MinStack {
- Stack<Integer> s;
- Stack<Integer>min;
- int val;
-    public MinStack() {
-        s=new Stack<>();
-        min=new Stack<>();
-    }
+// class MinStack {  //approch 1
+//  Stack<Integer> s;
+//  Stack<Integer>min;
+//  int val;
+//     public MinStack() {
+//         s=new Stack<>();
+//         min=new Stack<>();
+//     }
     
-    public void push(int value) {
-        s.push(value);
+//     public void push(int value) {
+//         s.push(value);
 
-        if(min.isEmpty()||min.peek()>=s.peek())
-            min.push(value);
-    }
+//         if(min.isEmpty()||min.peek()>=s.peek())
+//             min.push(value);
+//     }
     
-    public void pop() {
-         if(!s.isEmpty())
-          val=s.pop();
-        if(min.peek()==val)
-            min.pop();
+//     public void pop() {
+//          if(!s.isEmpty())
+//           val=s.pop();
+//         if(min.peek()==val)
+//             min.pop();
         
-    }
+//     }
     
-    public int top() {
-        if(s.isEmpty()) return -1;
-        return s.peek();
-    }
+//     public int top() {
+//         if(s.isEmpty()) return -1;
+//         return s.peek();
+//     }
     
-    public int getMin() {
-        if(!min.isEmpty()) return min.peek();
-        return -1;
-    }
-}
+//     public int getMin() {
+//         if(!min.isEmpty()) return min.peek();
+//         return -1;
+//     }
+// }
 
 /**
  * Your MinStack object will be instantiated and called as such:
@@ -43,25 +43,35 @@ class MinStack {
  */
 
 
-//  class MinStack {
-
-//     public MinStack() {
-        
-//     }
+ class MinStack {
+    int min;
+    Stack<Integer> s;
+    public MinStack() {
+        s=new Stack<>();
+        min=Integer.MAX_VALUE;
+    }
     
-//     public void push(int value) {
-        
-//     }
+    public void push(int value) {
+       if(value<=min)
+       {
+         s.push(min);
+         min=value;
+       }
+       s.push(value);
+    }
     
-//     public void pop() {
-        
-//     }
+    public void pop() {
+       if(s.pop()==min)
+        min=s.pop();
+       
+    }
     
-//     public int top() {
-        
-//     }
+    public int top() {
+        if(!s.isEmpty()) return s.peek();
+        return -1;
+    }
     
-//     public int getMin() {
-        
-//     }
-// }
+    public int getMin() {
+        return min;
+    }
+}
