25df0c25b717be9b688abc09a4043627ec25c006
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-08-04 10:06:26 +0530

    Time: 0 ms (100%), Space: 44.7 MB (67.3%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,13 +1,26 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int n=nums.length;
-        for(int i=0;i<n;i++)
+        // int n=nums.length;//normal loop ;//next time try binary search
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(nums[i]>=target)
+        //     {
+        //         return i;
+        //     }
+        // }
+        // return n;
+
+        int l=0,h=nums.length-1;
+        while(l<=h)
         {
-            if(nums[i]>=target)
-            {
-                return i;
-            }
+            int mid=l+(h-l)/2; 
+            if(nums[mid]==target) return mid;
+           else if(nums[mid]<target)
+                {
+                    l=mid+1;
+                }
+            else h=mid-1;
         }
-        return n;
+      return l;
     }
 }
