33444771dd29b730d09e5b169abf91abd43f3996
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-08-05 13:49:07 +0530

    Time: 1 ms (100%), Space: 47.2 MB (76.84%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,23 +1,47 @@
 
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        for(int i=0;i<nums.length;i++){
-            nums[i]=nums[i]*nums[i];
-        }
-        // Arrays.sort(nums);
-        int n=nums.length;
-        for(int i=0;i<n-1;i++) //bubble sort
+        // for(int i=0;i<nums.length;i++){   //brute force
+        //     nums[i]=nums[i]*nums[i];
+        // }
+        // // Arrays.sort(nums);
+        // int n=nums.length;
+        // for(int i=0;i<n-1;i++) //bubble sort
+        // {
+        //     for(int j=0;j<n-1-i;j++)
+        //     {
+        //       if(nums[j]>nums[j+1])
+        //       {
+        //         int temp=nums[j];
+        //         nums[j]=nums[j+1];
+        //         nums[j+1]=temp;
+        //       }
+        //     }
+        // }
+        // return nums;
+
+
+        int n=nums.length;              //brute force with space O(n) and time O(n)
+        int i=0,j=n-1,k=n-1;
+        int[] result=new int[n];
+        while(i<=j)
         {
-            for(int j=0;j<n-1-i;j++)
-            {
-              if(nums[j]>nums[j+1])
-              {
-                int temp=nums[j];
-                nums[j]=nums[j+1];
-                nums[j+1]=temp;
-              }
-            }
+            int leftside=nums[i]*nums[i];
+            int rightside=nums[j]*nums[j];
+
+            if(leftside>=rightside)
+          {
+            result[k]=leftside;
+            i++;
+          }
+          else
+          {
+            result[k]=rightside;
+            j--;
+          }
+          k--;
         }
-        return nums;
+        return result;
+
     }
 }
