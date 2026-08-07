1bcc4bcbb03d3a4dd83dd2c56f031677a11a2b1c
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-08-07 11:43:30 +0530

    Time: 24 ms (53.73%), Space: 69.2 MB (45.18%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.java
@@ -1,41 +1,41 @@
 class Solution {
     public int maxOperations(int[] nums, int k) {
-        // Arrays.sort(nums);
-        // int p=0,q=nums.length-1;
-        // int c=0;
-        // while(p<q)
-        // {
-        //     if(nums[p]+nums[q]==k)
-        //     {
-        //         c++;
-        //         p++;
-        //         q--;
-        //     }
-        //     else if(nums[p]+nums[q]<k)
-        //     {
-        //         p++;
-        //     }
-        //     else
-        //     {
-        //         q--;
-        //     }
-        // }
-        // return c;
-
+        Arrays.sort(nums);
+        int p=0,q=nums.length-1;
         int c=0;
-        HashMap<Integer,Integer> map=new HashMap<>();
-        for(int ele:nums)
+        while(p<q)
         {
-            if(map.containsKey(k-ele)&& map.get(k-ele)>0)
+            if(nums[p]+nums[q]==k)
             {
                 c++;
-                map.put(k-ele,map.get(k-ele)-1);
+                p++;
+                q--;
+            }
+            else if(nums[p]+nums[q]<k)
+            {
+                p++;
             }
             else
             {
-                map.put(ele,map.getOrDefault(ele,0)+1);
+                q--;
             }
         }
         return c;
+
+        // int c=0;
+        // HashMap<Integer,Integer> map=new HashMap<>();
+        // for(int ele:nums)
+        // {
+        //     if(map.containsKey(k-ele)&& map.get(k-ele)>0)
+        //     {
+        //         c++;
+        //         map.put(k-ele,map.get(k-ele)-1);
+        //     }
+        //     else
+        //     {
+        //         map.put(ele,map.getOrDefault(ele,0)+1);
+        //     }
+        // }
+        // return c;
     }
 }
