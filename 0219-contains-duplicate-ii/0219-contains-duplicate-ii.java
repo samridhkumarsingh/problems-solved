0e2150b58f47658c1363668d7dabf9c21e90cb9a
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-08-10 13:08:41 +0530

    Time: 29 ms (26.73%), Space: 91.9 MB (41.4%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,20 +1,37 @@
-class Solution {
+// class Solution {   //use of hashMap only ,, no use of sliding window
+//     public boolean containsNearbyDuplicate(int[] nums, int k) {
+//         int n=nums.length;
+//         HashMap<Integer,Integer> hm=new HashMap<>();
+//         for(int i=0;i<n;i++)
+//         {
+//             if(hm.containsKey(nums[i]))
+//             {
+//                 int x=hm.get(nums[i]);
+//                 if(Math.abs(x-i)<=k) return true;
+//                 else hm.put(nums[i],i);
+//             }
+//             else
+//             {
+//                 hm.put(nums[i],i);
+//             }
+//         }
+//         return false;
+//     }
+// }
+
+ class Solution {   //use of hashset and sliding window
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         int n=nums.length;
-        HashMap<Integer,Integer> hm=new HashMap<>();
-        for(int i=0;i<n;i++)
+        HashSet<Integer> set=new HashSet<>();
+        for(int r=0;r<n;r++)
         {
-            if(hm.containsKey(nums[i]))
+            if (set.contains(nums[r])) return true;
+            set.add(nums[r]);
+            if(r>=k)
             {
-                int x=hm.get(nums[i]);
-                if(Math.abs(x-i)<=k) return true;
-                else hm.put(nums[i],i);
-            }
-            else
-            {
-                hm.put(nums[i],i);
+                set.remove(nums[r-k]);
             }
         }
         return false;
     }
-}
+ }
