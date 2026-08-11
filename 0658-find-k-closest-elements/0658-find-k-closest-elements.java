aae5a42c4b39483693acbe6014609e2ff1cdeb16
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-08-11 15:08:58 +0530

    Time: 12 ms (41.01%), Space: 47.3 MB (98.7%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,57 +1,52 @@
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        List<Integer> rsl=new ArrayList<>(k);
-        int lo=0,hi=arr.length -1;
-        int lb=arr.length;
-        while(lo<=hi)
-        {
-            int mid=lo+(hi-lo)/2;
-            if(arr[mid]>=x)
+        List<Integer> list=new ArrayList<>();
+        int mid=Binary(arr,x);
+        int p=mid,q=mid+1;
+        while(k>0)
+        { 
+            if(p>=0 && q<arr.length)
             {
-                lb=Math.min(lb,mid);
-                hi=mid-1;
-            }
-            else
-            {
-                lo=mid+1;
-            }
-        }
-        
-       int p=lb-1;
-       int q=lb;
-        while(k-1>=0)
-        {
-            if(p>=0&&q<arr.length)
-            {
-                 if (Math.abs(arr[p] - x) <= Math.abs(arr[q] - x))
-                 {
-                    rsl.add(arr[p]);
-                     p--;
-                
-                  }
-                  else  
-                  {
-                   rsl.add(arr[q]);
-                     q++;
-              
-                  }
+                    if(Math.abs(x-arr[p])<=Math.abs(x-arr[q]))
+                {
+                    list.add(arr[p]);
+                    p--;
+                    
+                }
+                else
+                {
+                    list.add(arr[q]);
+                    q++;
+                }
             }
             else if(p>=0)
             {
-                rsl.add(arr[p]);
+                list.add(arr[p]);
                 p--;
-               
+              
             }
-            else 
+            else
             {
-                rsl.add(arr[q]);
+                list.add(arr[q]);
                 q++;
                
             }
-            k--;
+              k--;
         }
+        Collections.sort(list);
+        return list;
+    }
 
-    Collections.sort(rsl);
-    return rsl;
+    public int Binary(int[] arr,int x)
+    {
+         int le=0,rg=arr.length-1;
+        while(le<=rg)
+        {
+            int mid=le+(rg-le)/2;
+            if(arr[mid]==x) return mid;
+            else if(arr[mid]>x) rg=mid-1;
+            else le=mid+1; 
+        }
+        return rg;
     }
 }
