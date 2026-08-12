5986779212ca19fe39a3cce1ad3ed921cddd60d5
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-08-12 13:46:04 +0530

    Time: 29 ms (78.88%), Space: 95.5 MB (52.04%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,47 +1,47 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
-        int lng=0;
-        int count=1;
-        if(nums.length==0) return 0;
-        int lastmin=Integer.MIN_VALUE;
-        Arrays.sort(nums);
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i]-1==lastmin)
-            {
-                count++;
-                lastmin=nums[i];
-            }
-            else if(nums[i]-1!=lastmin && nums[i]!=lastmin)
-            {
-                lastmin=nums[i];
-                lng=Math.max(lng,count);
-                count=1;
-            }
-        }
-        lng=Math.max(lng,count);
-        return lng;
-        // HashSet <Integer> keys=new HashSet<>();
         // int lng=0;
-        // for(int i:nums)
+        // int count=1;
+        // if(nums.length==0) return 0;
+        // int lastmin=Integer.MIN_VALUE;
+        // Arrays.sort(nums);
+        // for(int i=0;i<nums.length;i++)
         // {
-        //     keys.add(i);
-        // }
-        // for(int i:keys)
-        // {
-        //     if(!keys.contains(i-1))
+        //     if(nums[i]-1==lastmin)
         //     {
-        //         int count=1;
-        //         int curr=i;
-        //         while(keys.contains(curr+1))
-        //         {
-        //             count++;
-        //             curr++;
-        //         }
-        //         lng=Math.max(count,lng);
+        //         count++;
+        //         lastmin=nums[i];
         //     }
-           
+        //     else if(nums[i]-1!=lastmin && nums[i]!=lastmin)
+        //     {
+        //         lastmin=nums[i];
+        //         lng=Math.max(lng,count);
+        //         count=1;
+        //     }
         // }
-        //  return lng;
+        // lng=Math.max(lng,count);
+        // return lng;
+        HashSet <Integer> keys=new HashSet<>();
+        int lng=0;
+        for(int i:nums)
+        {
+            keys.add(i);
+        }
+        for(int i:keys)
+        {
+            if(!keys.contains(i-1))
+            {
+                int count=1;
+                int curr=i;
+                while(keys.contains(curr+1))
+                {
+                    count++;
+                    curr++;
+                }
+                lng=Math.max(count,lng);
+            }
+           
+        }
+         return lng;
     }
 }
