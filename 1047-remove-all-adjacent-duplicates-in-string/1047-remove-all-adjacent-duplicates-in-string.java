4bc189b7942d477d391d8aa10b03ecc0aef318ca
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-08-15 02:04:59 +0530

    Time: 11 ms (90.76%), Space: 47.2 MB (52.05%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -1,31 +1,54 @@
+// class Solution {
+//     public String removeDuplicates(String s) {
+//         if(s.length()<=1) return s;
+//         StringBuilder str=new StringBuilder(s);
+      
+//         int p=0,q=1;
+//         while(q<str.length())
+//         {
+//             if(str.charAt(p)==str.charAt(q))
+//             {
+//                 // str.deleteCharAt(p);  //order of delete will matter  When you delete p first, everything after p shifts one position left. So q changes.
+//                 // str.deleteCharAt(q);
+
+//                 str.deleteCharAt(q);  //deleteCharAt() can take O(n) because characters after the deleted position may need to shift.
+//                 str.deleteCharAt(p);
+//                 if(p>0)
+//                 {
+//                     p--;
+//                 }
+//                 q=p+1;
+//             }
+//             else 
+//             {
+//                 p++;
+//                 q++;
+//             }
+//         }
+//         return str.toString();
+        
+//     }
+// }
+
+//So in the worst case, your solution can become: Time: O(n²) Space: O(n)
+
 class Solution {
     public String removeDuplicates(String s) {
-        if(s.length()<=1) return s;
-        StringBuilder str=new StringBuilder(s);
-      
-        int p=0,q=1;
-        while(q<str.length())
-        {
-            if(str.charAt(p)==str.charAt(q))
-            {
-                // str.deleteCharAt(p);  //order of delete will matter  When you delete p first, everything after p shifts one position left. So q changes.
-                // str.deleteCharAt(q);
+        StringBuilder str=new StringBuilder();
 
-                str.deleteCharAt(q);
-                str.deleteCharAt(p);
-                if(p>0)
-                {
-                    p--;
-                }
-                q=p+1;
+        for(char ch:s.toCharArray())
+        {
+            int n= str.length();
+            if(n>0 && str.charAt(n-1)==ch)
+            {
+                str.deleteCharAt(n-1);
             }
             else 
             {
-                p++;
-                q++;
+                str.append(ch);
             }
         }
+
         return str.toString();
-        
     }
 }
