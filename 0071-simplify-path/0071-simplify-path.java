f974fc48c0cb56d8fd5e3a6f2d7f974d4668c96c
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-08-17 11:36:09 +0530

    Time: 4 ms (96.6%), Space: 44.8 MB (76.67%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.java
+++ 0071-simplify-path/0071-simplify-path.java
@@ -1,47 +1,85 @@
+// class Solution {
+//     public String simplifyPath(String path) {
+//         Stack<String> st = new Stack<>();
+
+//         int p = 0;
+
+//         while (p < path.length()) {
+
+//             // skip '/'
+//             if (path.charAt(p) == '/') {
+//                 p++;
+//                 continue;
+//             }
+
+//             StringBuilder sb = new StringBuilder();
+
+//             // build one directory
+//             while (p < path.length() && path.charAt(p) != '/') {
+//                 sb.append(path.charAt(p));
+//                 p++;
+//             }
+
+//             String dir = sb.toString();
+
+//             if (dir.equals(".")) {
+//                 // do nothing
+//             }
+//             else if (dir.equals("..")) {
+//                 if (!st.isEmpty()) {
+//                     st.pop();
+//                 }
+//             }
+//             else {
+//                 st.push(dir);
+//             }
+//         }
+
+//         StringBuilder ans = new StringBuilder();
+
+//         for (String dir : st) {
+//             ans.append("/").append(dir);
+//         }
+
+//         if(ans.length() == 0 )return "/" ;
+//         return ans.toString();
+//     }
+// }
+
+//new --------------------------------
+
 class Solution {
     public String simplifyPath(String path) {
+        String[] components = path.split("/");
         Stack<String> st = new Stack<>();
 
-        int p = 0;
-
-        while (p < path.length()) {
-
-            // skip '/'
-            if (path.charAt(p) == '/') {
-                p++;
+        for (String comp : components) {
+            if (comp.equals("") || comp.equals(".")) {
                 continue;
             }
 
-            StringBuilder sb = new StringBuilder();
-
-            // build one directory
-            while (p < path.length() && path.charAt(p) != '/') {
-                sb.append(path.charAt(p));
-                p++;
-            }
-
-            String dir = sb.toString();
-
-            if (dir.equals(".")) {
-                // do nothing
-            }
-            else if (dir.equals("..")) {
+            if (comp.equals("..")) {
                 if (!st.isEmpty()) {
                     st.pop();
                 }
-            }
-            else {
-                st.push(dir);
+            } else {
+                st.push(comp);
             }
         }
 
-        StringBuilder ans = new StringBuilder();
+        StringBuilder sb = new StringBuilder();
+
+//         while (!st.isEmpty()) {
+//             sb.insert(0, "/" + st.pop());
+//         }
+
+//         return sb.length() == 0 ? "/" : sb.toString();   
 
         for (String dir : st) {
-            ans.append("/").append(dir);
-        }
+                    sb.append("/").append(dir);
+                }
 
-        if(ans.length() == 0 )return "/" ;
-        return ans.toString();
+                if(sb.length() == 0 )return "/" ;
+                return sb.toString();
     }
 }
