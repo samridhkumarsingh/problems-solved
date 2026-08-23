f13cd4d032498ec978de71a336e84e8ae25868ff
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-08-24 02:37:18 +0530

    Time: 1 ms (99.9%), Space: 52.8 MB (45.44%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -9,48 +9,71 @@
  *     }
  * }
  */
+// public class Solution {  //my own thinking
+//     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+//         ListNode tempa=headA;
+//         ListNode tempb=headB;
+//         int al=0,bl=0;
+
+//         while(tempa!=null)
+//         {
+//             al++;
+//             tempa=tempa.next;
+//         }
+
+//          while(tempb!=null)
+//         {
+//             bl++;
+//             tempb=tempb.next;
+//         }
+//         tempa=headA;
+//         tempb=headB;
+//         if(al>bl)
+//         {
+//             while(al-bl>0)
+//             {
+//                 tempa=tempa.next;
+//                 al--;
+//             }
+//         }
+//         else
+//         {
+//             while(bl-al>0)
+//             {
+//                 tempb=tempb.next;
+//                 bl--;
+//             }
+//         }
+
+//         while(tempa!=tempb)
+//         {
+//             tempa=tempa.next;
+//             tempb=tempb.next;
+//         }
+//         // if(tempa==tempb) return tempa;
+//         // else return null;
+//         return tempa;
+//     }
+// }
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode tempa=headA;
-        ListNode tempb=headB;
-        int al=0,bl=0;
 
-        while(tempa!=null)
-        {
-            al++;
-            tempa=tempa.next;
-        }
+        ListNode pA = headA;
+        ListNode pB = headB;
 
-         while(tempb!=null)
-        {
-            bl++;
-            tempb=tempb.next;
-        }
-        tempa=headA;
-        tempb=headB;
-        if(al>bl)
-        {
-            while(al-bl>0)
-            {
-                tempa=tempa.next;
-                al--;
-            }
-        }
-        else
-        {
-            while(bl-al>0)
-            {
-                tempb=tempb.next;
-                bl--;
-            }
+        while (pA != pB) {
+
+            if (pA == null)
+                pA = headB;
+            else
+                pA = pA.next;
+
+            if (pB == null)
+                pB = headA;
+            else
+                pB = pB.next;
         }
 
-        while(tempa!=tempb)
-        {
-            tempa=tempa.next;
-            tempb=tempb.next;
-        }
-        if(tempa==tempb) return tempa;
-        else return null;
+        return pA;
     }
 }
