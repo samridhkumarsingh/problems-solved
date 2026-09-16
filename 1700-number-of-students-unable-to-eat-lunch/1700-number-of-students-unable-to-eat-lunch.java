f7d63171ef38ed82a1f3a764b24bd34128599a26
samridh kumar singh <144904556+samridhkumarsingh@users.noreply.github.com>
2026-09-17 01:31:06 +0530

    Time: 0 ms (100%), Space: 43.3 MB (42.91%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.java
@@ -7,12 +7,25 @@ class Solution {
             if(x==0) Zero++;
             else One++;
         }
+        // for(int x:sandwiches)
+        // {
+        //     if(x==0 && Zero>0) Zero--;
+        //     else if(x==1 && One>0) One--;
+        //     else if(x==0 && Zero==0) return One;
+        //     else return Zero;
+        // }
         for(int x:sandwiches)
         {
-            if(x==0 && Zero>0) Zero--;
-            else if(x==1 && One>0) One--;
-            else if(x==0 && Zero==0) return One;
-            else return Zero;
+            if(x==0)
+            {
+                if(Zero==0) return One;
+                else Zero--;
+            }
+            else if(x==1)
+            {
+                if(One==0) return Zero;
+                else One--;
+            }
         }
         return 0;
     }
